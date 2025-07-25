bb6f3312a483377de350a96b44106ea4d6bacbf6
shailesh-00git <sy468698@gmail.com>
2025-07-25 16:32:07 +0545

    if-statements in js

---
+++ chapter2_control flow/ifstatements/index.js
@@ -0,0 +1,22 @@
+// if statement
+let age = 20;
+if (age > 18) {
+  console.log("You are eligible for driving");
+}
+
+let password = "pas23";
+if (password.length > 8) {
+  console.log("yes,its correct");
+} else {
+  console.log("Your password must be 8 digit");
+}
+
+//else if
+let city = "pokh";
+if (city == "kathmandu") {
+  console.log("You lives in kathmandu");
+} else if (city == "pokhara") {
+  console.log("You lives in pokhara");
+} else {
+  console.log("Where do you live");
+}
