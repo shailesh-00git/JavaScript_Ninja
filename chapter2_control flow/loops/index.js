a65a7364e5df6f80bc3d7413304bc6566e586d37
shailesh-00git <sy468698@gmail.com>
2025-07-25 15:58:37 +0545

    loops in js

---
+++ chapter2_control flow/loops/index.js
@@ -0,0 +1,41 @@
+// for loop
+// for (let i = 0; i < 5; i++) {
+//   console.log("This is loop: " + i);
+// }
+// console.log("loop finished");
+
+//  array and loop
+// let names = ["shailesh", "shreejan", "sam", "jhon"];
+// for (let i = 0; i < names.length; i++) {
+//   console.log(names[i]);
+// }
+
+//while loop
+// let i = 0;
+// while (i < 5) {
+//   console.log("This is loop: " + i);
+//   i++;
+// }
+
+//while loop with arrays
+// let names = ["saun", "luigi", "indess", "martin"];
+// let i = 0;
+// while (i < names.length) {
+//   console.log(names[i]);
+//   i++;
+// }
+
+//do-while loop
+// let i = 0;
+// do {
+//   console.log("This is loop: " + i);
+//   i++;
+// } while (i < 5);
+// console.log("Loop finished");
+let i = 0;
+const names = ["alex", "martin", "sam"];
+do {
+  console.log(names[i]);
+  i++;
+} while (i < names.length);
+console.log("Loop finished");
