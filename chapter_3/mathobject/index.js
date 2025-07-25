39f9b6ec03de99ad4f54001c78cb0e4b3da41177
shailesh-00git <sy468698@gmail.com>
2025-07-25 21:55:34 +0545

    Added math object

---
+++ chapter_3/mathobject/index.js
@@ -0,0 +1,17 @@
+//math object
+console.log(Math);
+
+//constant value in math object
+console.log(Math.PI);
+console.log(Math.E);
+
+//some other props
+let area = 3.5;
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.round(area));
+
+//random
+console.log(Math.random());
+console.log(Math.random() * 10);
+console.log(Math.floor(Math.random() * 100));
