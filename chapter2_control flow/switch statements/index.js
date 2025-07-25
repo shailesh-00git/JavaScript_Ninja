65f3f4d03088c98e809cea2e502bf361f8174b74
shailesh-00git <sy468698@gmail.com>
2025-07-25 18:22:20 +0545

    switch statements

---
+++ chapter2_control flow/switch statements/index.js
@@ -0,0 +1,21 @@
+// switch statements
+let grade = "B";
+switch (grade) {
+  case "A":
+    console.log("You got A grade");
+    break;
+  case "B":
+    console.log("You got B grade");
+    break;
+  case "C":
+    console.log("You got C grade");
+    break;
+  case "D":
+    console.log("You got D grade");
+    break;
+  case "E":
+    console.log("You got E grade");
+    break;
+  default:
+    console.log("Enter valid Grade");
+}
