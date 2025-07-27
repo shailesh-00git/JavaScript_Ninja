2b2e00a11531b2ae564993e23c1b2224d68a9a43
shailesh-00git <sy468698@gmail.com>
2025-07-27 20:52:15 +0545

    Added practise 1

--- chapter_4/practise1/index.js
+++ chapter_4/practise1/index.js
@@ -4,7 +4,12 @@ paras.forEach((element) => {
   if (element.textContent.includes("error")) {
     element.classList.add("error");
   }
-   if (element.textContent.includes("success")) {
+  if (element.textContent.includes("success")) {
     element.classList.add("success");
   }
 });
+
+let heading = document.querySelector(".heading-primary");
+console.log(heading);
+heading.classList.toggle("heading-secondary");
+heading.classList.toggle("heading-secondary");
