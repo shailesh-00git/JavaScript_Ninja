ef8efec73ff30e2091b2f911a39643db85968d3b
shailesh-00git <sy468698@gmail.com>
2025-08-07 22:45:11 +0545

    Addedd digital clock

---
+++ Digital clock/index.js
@@ -0,0 +1,15 @@
+//digital clock
+const clock = document.querySelector(".clock");
+setInterval(() => {
+  const now = new Date();
+  const h = now.getHours();
+  const m = now.getMinutes();
+  const s = now.getSeconds();
+
+  const html = `
+  <span>${h}</span> :
+  <span>${m}</span> :
+  <span>${s}</span>
+  `;
+  clock.innerHTML = html;
+}, 1000);
