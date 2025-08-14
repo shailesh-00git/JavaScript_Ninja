ee0000a9da75c512d874da7db37963a8942e9292
shailesh-00git <sy468698@gmail.com>
2025-08-14 22:25:44 +0545

    Added local storsge in js

---
+++ chapter_8/01-local storage/index.js
@@ -0,0 +1,16 @@
+//store data
+localStorage.setItem("name", "shailesh");
+localStorage.setItem("age", "20");
+localStorage.setItem("address", "sanepa");
+
+//get data
+const name = localStorage.getItem("name");
+
+//update data
+localStorage.setItem("name", "samrat");
+console.log(localStorage.getItem("name"));
+console.log(localStorage.getItem("age"));
+console.log(localStorage.getItem("address"));
+
+//delete item
+// localStorage.removeItem("address");
