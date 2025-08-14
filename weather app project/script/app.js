6ca78782e58300bec52a4b73595033aa43d814db
shailesh-00git <sy468698@gmail.com>
2025-08-14 22:06:40 +0545

    Weather app completed

--- weather app project/script/app.js
+++ weather app project/script/app.js
@@ -1,17 +1,21 @@
 const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
 
 //update ui function
 const updateUI = (data) => {
   const cityDetail = data.cityDetails;
   const weather = data.weather;
   // const { cityDetail, weather } = data;
-  console.log(cityDetail);
-  console.log(weather);
+  // console.log(cityDetail);
+  // console.log(weather);
+  // console.log(weather.WeatherIcon);
 
   //extract details of city
   const temp = weather.Temperature.Metric.Value;
+  // console.log(temp);
   const currWeather = weather.WeatherText;
   const name = cityDetail.EnglishName;
 
@@ -23,9 +27,31 @@ const updateUI = (data) => {
      <span>${temp}</span>
      <span>&deg;c</span>
      </div>`;
+
+  //updating image according to day or night
+
+  let iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
+  let timeSrc = null;
+
+  //use of ternary operator
+  timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg";
+  // if (weather.IsDayTime) {
+  //   timeSrc = "img/day.svg";
+  // } else {
+  //   timeSrc = "img/night.svg";
+  // }
+  time.setAttribute("src", timeSrc);
+
+  //adding the info after city search
   if (card.classList.contains("d-none")) {
     card.classList.remove("d-none");
   }
+  // Scroll smoothly to the card
+  card.scrollIntoView({
+    behavior: "smooth",
+    block: "end",
+  });
 };
 
 //getting city detail and weather
@@ -37,6 +63,7 @@ const updateCity = async (city) => {
     weather: weather,
   };
 };
+
 cityForm.addEventListener("submit", (e) => {
   //prevent default action
   e.preventDefault();
