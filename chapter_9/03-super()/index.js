6f0c00fb798427b7c01fa3ec25ab2da497637f19
shailesh-00git <sy468698@gmail.com>
2025-08-17 22:28:15 +0545

    Added super()

---
+++ chapter_9/03-super()/index.js
@@ -0,0 +1,28 @@
+////////////////class inheritance////////////////
+
+//class and constructor
+class User {
+  constructor(name, email) {
+    this.userName = name;
+    this.email = email;
+  }
+  login() {
+    console.log(`${this.userName} logged in`);
+  }
+  logout() {}
+}
+class Admin extends User {
+  constructor(username, email, title) {
+    super(username, email);
+    this.title = title;
+  }
+  deleteUser(user) {
+    users = users.filter((u) => {
+      return u.userName !== user.userName;
+    });
+  }
+}
+const userOne = new User("mario", "mario@gmail.com");
+const userTwo = new User("ryu", "ryu@gmail.com");
+const userThree = new Admin("shailesh", "shailesh.admin@info", "blue-belt");
+console.log(userThree);
