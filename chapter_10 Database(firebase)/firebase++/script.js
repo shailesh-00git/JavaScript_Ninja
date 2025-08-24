1b12c5362bee17975c9af832c2639f768941163a
shailesh-00git <sy468698@gmail.com>
2025-08-24 19:23:02 +0545

    realtime updates on page from database

--- chapter_10 Database(firebase)/firebase++/script.js
+++ chapter_10 Database(firebase)/firebase++/script.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import {
   getFirestore,
-  collection, // to get the whole collection
-  getDocs, //for retriving the docs
-  addDoc, //for adding docs
-  serverTimestamp, //adding the docs
-  deleteDoc, //deleting the docs
-  doc, //for getting the docs
+  collection,
+  addDoc,
+  serverTimestamp,
+  deleteDoc,
+  doc,
+  onSnapshot,
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 // Firebase config
@@ -23,67 +23,59 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Fetch recipes
-async function getRecipies() {
-  const recipiesCol = collection(db, "recipies");
-  const recipSnapshot = await getDocs(recipiesCol);
-  return recipSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })); //include id
-}
-
-//getting references to parent element
 const list = document.querySelector("ul");
 const form = document.querySelector("form");
 
-//Add recipie to html
-const addRecipe = (recipie, id) => {
-  let html = `<li data-id="${id}"><div>${
-    recipie.name
-    //toDate() converts mili and nano second in date
-  }</div><div>${recipie.createdAt.toDate()}</div>
-  <button class ="btn btn-outline-danger my-2">delete</button>
-  </li>`;
+// Add recipe to HTML
+const addRecipe = (recipe, id) => {
+  const createdAt = recipe.createdAt?.toDate() || new Date();
+
+  const html = `
+        <li data-id="${id}">
+          <div>${recipe.name}</div>
+          <div>${createdAt}</div>
+          <button class="btn btn-outline-danger my-2">delete</button>
+        </li>
+      `;
   list.innerHTML += html;
 };
 
-//extracting data from form adding to database
+// Delete recipe from DOM
+const deleteRecipeFromDOM = (id) => {
+  const item = document.querySelector(`li[data-id='${id}']`);
+  if (item) item.remove();
+};
+
+// Real-time listener (handles initial load + future changes)
+onSnapshot(collection(db, "recipies"), (snapshot) => {
+  snapshot.docChanges().forEach((change) => {
+    if (change.type === "added") {
+      addRecipe(change.doc.data(), change.doc.id);
+    }
+    if (change.type === "removed") {
+      deleteRecipeFromDOM(change.doc.id);
+    }
+  });
+});
+
+// Add recipe to Firestore
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const title = form.recipie.value.trim();
+  if (!title) return;
+
   addDoc(collection(db, "recipies"), {
     name: title,
     createdAt: serverTimestamp(),
   })
-    .then(() => {
-      // console.log("recipie sucessfuly deleted");
-    })
+    .then(() => form.reset())
     .catch((err) => console.log(err));
 });
-form.reset();
 
-//deleting the recipies
+// Delete recipe from Firestore
 list.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log(e);
-  if (e.target.tagName == "BUTTON") {
-    console.log("button was clicked");
+  if (e.target.tagName === "BUTTON") {
     const id = e.target.parentElement.getAttribute("data-id");
-    // console.log(id);
-    deleteDoc(doc(db, "recipies", id))
-      .then(() => {
-        // console.log("recipie sucessfully deleted");
-      })
-      .catch((err) => console.log(err));
+    deleteDoc(doc(db, "recipies", id)).catch((err) => console.log(err));
   }
-  // console.log(e.target.innerText);
-  // e.target.innerText = "deleting...";
 });
-
-getRecipies()
-  .then((recipiList) => {
-    recipiList.forEach((element) => {
-      // console.log(element.id);
-      addRecipe(element, element.id);
-      // console.log(element);
-    });
-  })
-  .catch((err) => console.error(err));
