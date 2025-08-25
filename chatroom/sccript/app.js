b73086b290976551fbdd422b1ed68b9e5b50ef6a
shailesh-00git <sy468698@gmail.com>
2025-08-25 17:05:38 +0545

    connectiong to datbase

---
+++ chatroom/sccript/app.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  serverTimestamp,
+  deleteDoc,
+  doc,
+  onSnapshot,
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyBRYd-3WaAc4nr6qSbD6AweWse3CbUpeCo",
+  authDomain: "ninja-test-754f5.firebaseapp.com",
+  databaseURL: "https://ninja-test-754f5-default-rtdb.firebaseio.com",
+  projectId: "ninja-test-754f5",
+  storageBucket: "ninja-test-754f5.appspot.com",
+  messagingSenderId: "567885343662",
+  appId: "1:567885343662:web:18f4168cc413644ff9f63f",
+  measurementId: "G-JKJ1JKZTXH",
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+onSnapshot(collection(db, "recipies"), (snapshot) => {
+  console.log(snapshot.docChanges());
+});
