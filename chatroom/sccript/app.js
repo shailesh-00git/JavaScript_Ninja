7c79c3a90444d09eb3fbe85e8a5fdf8dc4840ce9
shailesh-00git <sy468698@gmail.com>
2025-08-27 21:26:04 +0545

    Added real time liostemers

--- chatroom/sccript/app.js
+++ chatroom/sccript/app.js
@@ -7,6 +7,9 @@ import {
   deleteDoc,
   doc,
   onSnapshot,
+  query,
+  where,
+  orderBy,
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 // Firebase config
@@ -20,9 +23,64 @@ const firebaseConfig = {
   appId: "1:567885343662:web:18f4168cc413644ff9f63f",
   measurementId: "G-JKJ1JKZTXH",
 };
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-onSnapshot(collection(db, "recipies"), (snapshot) => {
-  console.log(snapshot.docChanges());
+// ✅ Chatroom class
+class Chatroom {
+  constructor(room, username) {
+    this.room = room;
+    this.username = username;
+    this.chats = collection(db, "chats");
+    this.unsub;
+  }
+
+  // add new chat
+  async addChat(message) {
+    const chat = {
+      message,
+      username: this.username,
+      room: this.room,
+      createdAt: serverTimestamp(), // ✅ better than Timestamp.fromDate(new Date())
+    };
+    return await addDoc(this.chats, chat);
+  }
+
+  // listen for new chats
+  getChats(callback) {
+    const unsub =()=>{
+    const q = query(
+      this.chats,
+      where("room", "==", this.room),
+      orderBy("createdAt") // ✅ needs composite index
+    );
+
+    this.unsub =  onSnapshot(q, (snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === "added") {
+          callback(change.doc.data());
+        }
+      });
+    });
+  }
+  updateUsername(username) {
+    this.username = username;
+  }
+  updateRoom(room) {
+    this.room = room;
+    console.log("room updated");
+    if(this.unsub){
+      this.unsub()
+    }
+  }
+}
+}
+
+const chatroom = new Chatroom("general", "shailesh");
+
+// listen for real-time updates
+chatroom.getChats((data) => {
+  console.log(data);
 });
+chatroom.updateRoom("gaming");
