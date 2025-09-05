b4ec69f9f4b5abfe8a5656d140ebad9e45f62acf
shailesh-00git <sy468698@gmail.com>
2025-09-05 20:08:00 +0545

    UI-libray completed

--- chapter_14 UI-library/src/index.js
+++ chapter_14 UI-library/src/index.js
@@ -1,7 +1,7 @@
 import Tooltip from "./ninja-ui/tooltip";
 import DropDown from "./ninja-ui/dropdown";
 import Tabs from "./ninja-ui/tabs";
-
+import SnackBar from "./ninja-ui/snackbar";
 //craete a tooltip
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
 tooltip.inti();
@@ -16,3 +16,11 @@ dropdowns.forEach((dropdown) => {
 // create  tabs
 const tabs = new Tabs(document.querySelector(".tabs"));
 tabs.init();
+
+//create snack bar
+const snackbar = new SnackBar();
+snackbar.init();
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  snackbar.show("you clicked me 😊");
+});
---
+++ chapter_14 UI-library/src/ninja-ui/snackbar.js
@@ -0,0 +1,18 @@
+import "./styles/snackbar.css";
+class SnackBar {
+  constructor() {
+    this.snackbar = document.createElement("div");
+  }
+  init() {
+    this.snackbar.classList.add("snackbar");
+    document.querySelector("body").appendChild(this.snackbar);
+  }
+  show(message) {
+    this.snackbar.textContent = message;
+    this.snackbar.classList.add("active");
+    setTimeout(() => {
+      this.snackbar.classList.remove("active");
+    }, 3000);
+  }
+}
+export { SnackBar as default };
